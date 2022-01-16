ad55846de11e6a9386604fdc7a4ab32aa049ea09
lukee90000 <thomas.l.james@students.plymouth.ac.uk>
2022-01-16 22:07:29 +0000

    Canvas Created

--- Below/BelowJavaScript.js
+++ Below/BelowJavaScript.js
@@ -1,1 +1,9 @@
-
+function draw() {
+    var ctx = backgroundCanvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
+    draw();
+}
+draw();
