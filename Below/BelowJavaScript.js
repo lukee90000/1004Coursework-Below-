00645b4f0913355c3899bd03d03f1ca95036eb77
lukee90000 <thomas.l.james@students.plymouth.ac.uk>
2022-01-16 22:30:08 +0000

    Surface Created

--- Below/BelowJavaScript.js
+++ Below/BelowJavaScript.js
@@ -1,9 +1,25 @@
+var room = "surface";
+
+
+
+
+
+
 function draw() {
     var ctx = backgroundCanvas.getContext("2d");
+
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "black";
     ctx.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
-    draw();
+    if (room = "surface") {
+        ctx.beginPath();
+        ctx.strokeStyle = "brown";
+        ctx.fillStyle = "brown"
+        ctx.fillRect(0, 2 * backgroundCanvas.height / 3, backgroundCanvas.width, backgroundCanvas.height / 3);
+        ctx.stroke();
+    }
+
+    window.requestAnimationFrame(draw);
 }
 draw();
